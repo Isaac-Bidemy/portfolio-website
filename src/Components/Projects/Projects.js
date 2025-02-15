c2e8a75b26d94e43077cdcbf7f34aac0ba2a83f1
Isaac-Bidemy <isaacbidemi25@gmail.com>
2025-02-15 07:21:40 -0800

    updated

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,9 +4,9 @@ import React from 'react';
 
 
 import "./Projects.css";
-import Project1 from "../../assets/images/project1.png";
+import Project1 from "../../assets/images/project6.png";
 import Project5 from "../../assets/images/project4.png";
-import Project6 from "../../assets/images/project6.png";
+import Project6 from "../../assets/images/project1.png";
 import Project7 from "../../assets/images/wild.png";
 
 
